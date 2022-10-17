8e66649b2d0039a13f3cbde0a893a702446ab212
eliHeist <eliyang256@outlook.com>
2022-10-17 15:02:32 +0300

    done top header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: "rgb(0, 50, 160)"
+        },
+        secondary: {
+          500: "rgb(252, 175, 60)"
+        }
+      }
+    },
   },
   plugins: [],
 }
