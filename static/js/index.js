7ee04bdd71eb60fb676ed16f74175801fa81af63
Elijah Triumph <eliyang256@outlook.com>
2022-10-26 13:51:51 +0300

    done slider with gsap

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,47 @@
-let slides = document.getElementById('slides')
+//#region slider
+let slides = document.getElementById('slides').children
 
-console.log(slides.children[1].nextElementSibling);
+// console.log(slides);
+
+let currentSlide = 1000;
+let nextSlide = 0;
 
 
 function slide() {
-   const t1 = gsap.timeline({ paused: false, defaults: { Easings: Expo.EaseOut }, stagger: 6 })
+   next = slides[nextSlide]
+   current = slides[currentSlide]
 
-   t1.to(".slide", { clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)', zIndex: 2 })
-   .from(".text h2", { opacity: 0, duration: 2 })
+   const t1 = gsap.timeline({ paused: true, defaults: { Easings: Expo.EaseOut } })
+   
+   if (current) {
+      t1.to(current.querySelector('.fade-in'), { opacity: 0, scale: 0.1, duration: 0.5 })
+         .to(current.querySelector('button'), { opacity: 0, scale: 0.1, duration: 0.5 }, '+=0.1')
+         .to(current, { clipPath: 'polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)' })
+   }
+   t1.fromTo(next, { clipPath: 'polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)'}, { clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)', duration: 1 }, '-=1')
+      .to(next.querySelector('.fade-in'), { opacity: 1, scale: 1, duration: 0.5 }, '+=0.1')
+      .to(next.querySelector('button'), { opacity: 1, scale: 1, duration: 0.5 }, '+=0.1')
+      
    
    t1.play()
+   // switch next and previous slides
+   switchSlides()
+}
+
+function switchSlides() {
+   max = slides.length-1
+   if (nextSlide == max) {
+      currentSlide = nextSlide
+      nextSlide = 0
+   }
+   else {
+      currentSlide = nextSlide
+      nextSlide += 1
+   }
 }
 
 slide()
+setInterval(slide, 20000)
+//#endregion
+
+
